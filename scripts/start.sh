b53ec6174bba6989c0c7e47a28563831eb35df75
amosgansweet <171889997+amosgansweet@users.noreply.github.com>
2025-04-16 17:37:00 +0800

    Delete scripts directory

--- scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ps aux | grep -v grep | grep webh2 > /dev/null
-  if [ $? -ne 0 ]; then
-      echo "process 'run webh2' stop，starting..."
-      cd /home/aomega-yahai/domains/aomega-yahai.serv00.net/public_html
-      nohup /home/aomega-yahai/domains/aomega-yahai.serv00.net/public_html/webh2 server -c /home/aomega-yahai/domains/aomega-yahai.serv00.net/logs/web159.yaml >/dev/null 2>&1 &
-  fi
